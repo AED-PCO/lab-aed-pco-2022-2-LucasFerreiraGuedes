6ebfdb4b17fe5a59b99da416d09e224f61ac4cef
LucasFerreiraGuedes <lucasguedes01673874@gmail.com>
2022-10-07 15:06:59 -0300

    Padronização na saída do código

--- codigo/Exercs Lab 5/Exercs Ordenação/Insert Sort/Program.cs
+++ codigo/Exercs Lab 5/Exercs Ordenação/Insert Sort/Program.cs
@@ -1,33 +1,44 @@
-﻿
-
-namespace InsertoSort
+﻿namespace InsertonSort
 {
     class proram
     {
         static void Main(string[] args)
         {
-            int[] vet = new int[] { 23, 16, 4, 42, 8, 15 };
-            imprimeVetor(Insert(vet));
+            int[] vet = new int[10];
+            preencheVetor(vet);
 
+            Console.WriteLine("Vetro Desordenado: ");
+            imprimeVetor(vet);
+
+            Console.WriteLine("Vetor ordenado: ");
+            int troca = 0,comparacao=0;
+            imprimeVetor(Insert(vet,ref troca,ref comparacao));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Troca: {troca},  Comparação: {comparacao}");
+ 
         }
-        static int [] Insert(int[] vet)
+        static int[] Insert(int[] vet,ref int troca,ref int comparacao)
         {
             int menor = 0;
-
-            for (int i = 1 ; i <= vet.Length-1; i++)
+ 
+            for (int i = 1; i <= vet.Length - 1; i++)
             {
                 menor = vet[i];
-
-                for (int j = i;j >= 1; j--)
-                    {
+ 
+                for (int j = i; j >= 1 && vet[j] < vet[j-1] ; j--)
+                {
+                    comparacao++;
+ 
                     if (vet[j] < vet[j - 1])
                     {
                         menor = vet[j];
                         vet[j] = vet[j - 1];
                         vet[j - 1] = menor;
+                        troca ++;
                     }
-                    
-                    }
+ 
+                }
             }
             return vet;
         }
@@ -37,7 +48,19 @@ namespace InsertoSort
             {
                 Console.Write("\t" + vet[i]);
             }
+            Console.WriteLine();
+        }
+         static void preencheVetor(int[] vet)
+        {
+            Random randNum = new Random();
+            for (int i = 0; i < vet.Length; i++)
+            {
+                vet[i] = randNum.Next(10);
+            }
         }
 
+ 
     }
 }
+
+
