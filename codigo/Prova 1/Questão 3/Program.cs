e20d79a739f7537a2a5b82720f6977851ff14054
LucasFerreiraGuedes <lucasguedes01673874@gmail.com>
2022-10-27 17:04:27 -0300

    Questão 3 da 1º Prova

---
+++ codigo/Prova 1/Questão 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Modelo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            EscrevaArquivo();
+        }
+    
+      static void EscrevaArquivo()
+        {
+            StreamReader SR = new StreamReader("C:\\Users\\alefa\\OneDrive\\Área de Trabalho\\AED Exercs\\QuestoesProva1\\Questao_3\\entrada.txt");
+
+          
+             string PrimeiraL = SR.ReadToEnd();
+            char[] arr = PrimeiraL.ToCharArray();
+            //char[] arr  = SR.ReadToEnd().ToCharArray(); também há esta forma mais direta de fazer !!
+            SR.Close();
+
+            StreamWriter SW = new StreamWriter("C:\\Users\\alefa\\OneDrive\\Área de Trabalho\\AED Exercs\\QuestoesProva1\\Questao_3\\saida.txt");
+            for (int i = arr.Length-1; i >= 0; i--)
+            { 
+                if (arr[i] == ' ')
+                {
+                    SW.Write(" ");
+                }
+                else
+                 SW.Write(arr[i]);
+            }
+            SW.Close();
+
+
+
+        }
+    
+    
+    
+    
+    }
+}
+
