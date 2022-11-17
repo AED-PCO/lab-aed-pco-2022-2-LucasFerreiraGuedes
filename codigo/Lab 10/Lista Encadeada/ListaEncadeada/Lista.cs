722eb4dd43042bdcb9d1a03c3142859777afe04b
LucasFerreiraGuedes <lucasguedes01673874@gmail.com>
2022-11-17 16:18:22 -0300

    Corriginddo bugs de Remoção - Lista Encadeada

--- codigo/Lab 10/Lista Encadeada/ListaEncadeada/Lista.cs
+++ codigo/Lab 10/Lista Encadeada/ListaEncadeada/Lista.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+ 
 namespace ListaEncadeada
 {
     internal class Lista
     {
         public Elemento Primeiro = null;
-
+ 
         public Elemento Ultimo = null;
-
+ 
         public int Contador = 0;
-
+ 
         public Lista()
         {
-
+ 
         }
-
+ 
         public void InsercaoLinear(Elemento elemento)
         {
             if (Primeiro == null)
@@ -33,109 +33,111 @@ namespace ListaEncadeada
                 Ultimo = elemento;
                 Contador++;
             }
-
+ 
         }
-
+ 
         public void InsercaoPos(Elemento elemento, int pos)
         {
-            var aux = Primeiro;
-
+            Elemento aux = Primeiro;
             if (pos <= Contador)
             {
-                for (int i = 0; i < pos; i++)  // Aqui eu sobrescrevo um elemento de acordo com o pos passado, parando uma posição a frente e referenciando utilizando o .proximo
+                for (int i = 1; i < pos; i++)  // Aqui eu sobrescrevo um elemento de acordo com o pos passado, parando uma posição a frente e referenciando utilizando o .proximo
                 {
                     aux = aux.proximo;
-
+                                              // Se eu quiser fazer o shift, eu coloco aux.proximo e i = 0
                 }
-                aux = elemento;
+                elemento.proximo = aux.proximo.proximo;
+                aux.proximo = elemento;
             }
             else
             {
-
+ 
                 InsercaoLinear(elemento);  //Quando meu pos é maior do que a pos presente na lista, eu o insiro no final dela.
             }
-
+ 
         }
-
-
-
+ 
+ 
+ 
         public void RemoveElemento(Elemento elemento)
         {
             Elemento aux = Primeiro;
-
+ 
             if (aux == elemento)
             {
                 aux = aux.proximo;
             }
             else
             {
-
-
                 while (aux != null)
                 {
-
+ 
                     if (aux.proximo == elemento)
                     {
                         aux.proximo = elemento.proximo;
-                        break;
+                        return;
                     }
                     else
                     {
                         aux = aux.proximo;
                     }
                 }
+                Console.WriteLine("O elemento não existe nesta Lista");
             }
-
-
+ 
+ 
         }
         public void RemoveUltimo()
         {
-
+ 
             var aux = Primeiro;
-           
-
+ 
+ 
             if (aux == null)
             {
-                throw new Exception("Não há itens para serem retirados");
+                Console.WriteLine("Não há itens para serem retirados");
             }
-
+            else if(Primeiro == Ultimo)            {
+                Primeiro = Ultimo = null;
+            }
             else
             {
-
-                while (aux.proximo != null)
+               
+                while (aux.proximo != Ultimo) // tem mais de um elemento na lista
                 {
                     aux = aux.proximo;
                 }
                 aux.proximo = null;
-
+                Ultimo = aux;    
             }
         }
-
-            public void ImprimeLista()
+ 
+        public void ImprimeLista()
+        {
+           
+ 
+            if (Primeiro == null)
             {
-                Console.WriteLine();
-             
-            if(Primeiro == null)
-            {
-                throw new Exception("Não existem itens nesta lista");
+                Console.WriteLine("Não existem itens nesta lista");
             }
-
+ 
             else
             {
                 var aux = Primeiro;
-
-
+ 
+ 
                 Console.Write(aux.aluno.Nome + "\t");
-
+ 
                 while (aux.proximo != null)
                 {
                     Console.Write(aux.proximo.aluno.Nome + "\t");
-
+ 
                     aux = aux.proximo;
                 }
             }
-                
-            }
-        
+ 
+            Console.WriteLine();
+ 
+        }
     }
 }
